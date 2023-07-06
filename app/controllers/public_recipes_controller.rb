f0274e2e1d03d947a42b750d68a57ea8bd72c690
Precious Betine <preciousbetine@gmail.com>
2023-07-06 14:42:55 +0100

    Design public recipes page

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,7 +1,8 @@
 class PublicRecipesController < ApplicationController
+  layout 'standard'
   before_action :authenticate_user!
 
   def index
-    @recipes = Recipe.where(public: true).order(created_at: :desc)
+    @recipes = Recipe.includes(:user).where(public: true).order(created_at: :desc)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,12 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:show]
+
   resources :foods
 
   resources :recipes, except: [:update] do
-    resources :foods_recipes, only: [:new, :create, :destroy]
-  end
-
-  resources :public_recipes, except: [:update]
-
-  resources :recipes do
     patch 'toggle_public', on: :member
   end
+
+  resources :public_recipes, only: [:index]
 end
