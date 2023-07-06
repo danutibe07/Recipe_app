c28a82810cae358dd8acdcfd6e188d8f6ebb4b9a
Precious Betine <preciousbetine@gmail.com>
2023-07-06 14:30:05 +0100

    Design recipe pages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,5 @@
 class RecipesController < ApplicationController
+  layout 'standard'
   before_action :authenticate_user!
 
   def index
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,16 +2,14 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user
+    user ||= User.new
 
-    return unless user.present? # additional permissions for logged in users
+    return unless user.present?
 
     can(:read, Recipe, user)
     can(:read, Food, user)
-    # can(:read, RecipeFood, user)
     can :update, Recipe, user_id: user.id
     can :destroy, Recipe, user_id: user.id
     can :destroy, Food, user_id: user.id
-    # can :destroy, RecipeFood, user_id: user.id
   end
 end
